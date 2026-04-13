39c353baa64416c5047502774db644c60f5617fe
raghavpandit84233-lgtm <raghavpandit84233@gmail.com>
2026-04-13 07:16:50 +0530

    Implement Selection Sort algorithm in C++
    code for selection sort using vector cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void SelectionSort(vector<int>& arr)
+{
+  int n = arr.size();
+  for(int i =0 ; i<arr.size() -1;i++)
+    {   int minIdx= i ;
+      for(int j = i+1 ; j<arr.size(); j++)
+        {
+          if(arr[j]<arr[minIdx])
+          {
+            minIdx=j;
+          }
+        }
+     swap(arr[i],arr[minIdx]);
+    }
+}
+int main ()
+{
+  int n ;
+  cout<<"Enter number of elements in the array : ";
+    cin>>n;
+  vector <int> arr;
+  cout<<"Enter elements : ";
+  for(int i=0 ; i<n;i++)
+    {
+      int x ;
+      cin>>x;
+      arr.push_back(x);  // adding elements
+    }
+   SelectionSort(arr);
+  cout<<"Sorted Array : ";
+  for(int i=0; i<arr.size();i++)
+    {
+      cout<<arr[i] <<" ";
+    }
+ 
+ return 0;  
+}
